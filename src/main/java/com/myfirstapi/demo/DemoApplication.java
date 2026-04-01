b560c38595e7ca7524aa2500a55223f32491323e
KINGFU05 <drkcorp01@gmail.com>
2026-04-01 16:32:12 -0400

    (done): i finish my first api

--- src/main/java/com/myfirstapi/demo/DemoApplication.java
+++ src/main/java/com/myfirstapi/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 	@GetMapping("/hello") // que es un getmapping
 	public String hello() {
-		return "Hello, World!";
+		return "Hello, World to my First API!";
 	}
 
 	public static void main(String[] args) {
